5852e2a93c73923c4ed385e2f56653773899ee80
nabilapranatya <pranatyanabila.com>
2023-08-07 14:08:19 +0700

    belajar-vuex-module

---
+++ src/store/modules/donatur.js
@@ -0,0 +1,21 @@
+const donatur = {
+    namespaced: true,
+    state: {
+        listDonatur: [
+            { name: "Riski Amelia"},
+            { name: "Ima Sumadir"},
+            { name: "Apri Yunita"},
+            { name: "Tuti Winarti"},
+            { name: "Muhammad Fauzi"},
+            { name: "Hamba Allah"},
+        ],
+    },
+    mutations: {
+        //NONE
+    },
+    actions: {
+        //NONE
+    },
+};
+
+export default donatur;
